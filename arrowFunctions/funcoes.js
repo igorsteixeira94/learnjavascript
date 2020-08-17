901c965ec2da5e60a57c8bc16bee62493a53b66b
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-08-17 11:16:49 -0300

    Feature: Parametros predefinidos em funções

--- arrowFunctions/funcoes.js
+++ arrowFunctions/funcoes.js
@@ -20,3 +20,19 @@ soma(3,4);
 var soma = (a,b) => console.log(`Esta soma é uma => ${a+b}`);
 
 soma(3,0)
+
+/**
+ * Parametros predefinidos em funções:
+ * - É possivel passar parametros com valores pré-definidos para funções.
+ * - Tbm podemos passar uma função, assim como o callback
+ */
+
+ function multiplica(a=0,b=0){
+   console.log(a*b);
+ }
+
+ function nome(nome='abc', callback = z =>{console.log(z)}){
+   callback(nome);
+ }
+
+ nome("Igor Rodrigues");
