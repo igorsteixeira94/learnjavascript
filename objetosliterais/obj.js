ffda018cad6eef08d9490e61c45bac1d29436f2a
Igor Rodrigues <igorsteixeira94@gmail.com>
2020-08-17 11:38:02 -0300

    Feature: Desestruturação

--- objetosliterais/obj.js
+++ objetosliterais/obj.js
@@ -20,3 +20,48 @@ const objeto ={
 }
 
 objeto.invocar();
+
+/**
+ * Desestruturação de arrays e objetos.
+ * Possivél usar a sintaxe de objeto litarais para extrair valores,
+ * Podemos desestruturar e objetos e dar apelidos a ele.
+ * Podemos desestruturar arrays
+ * Podemos desestruturar objetos e arrays.
+ */
+
+ const user={
+   nome:'Igor Rodrigues',
+   email:'igor@mail.com',
+   senha:'123',
+
+ }
+
+ const {nome,senha} = user;
+
+ console.log(nome,senha);
+
+const {email:usuario} = user;
+
+//console.log(email); //not defined
+console.log(usuario);
+
+const users=[
+  {
+  nome:['Igor', 'Rodrigues'],
+  email:'igor@mail.com',
+  senha:'123',
+  },
+  {
+    nome:['Iuri', 'Rodrigues'],
+    email:'iuri@mail.com',
+    senha:'321',
+  }
+];
+
+const [,{senha:pass}] = users;
+
+console.log(pass); // pegando a senha do segundo usuario
+
+const [{nome:[,sobrenome]}] = users;
+
+console.log(sobrenome);//Pega o sobrenome do primeiro usuario
